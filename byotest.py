4b9fa54bb73851617eeb9b49dfd451c8dfe450a3
Ubuntu <ubuntu@ip-172-31-94-185.ec2.internal>
2019-08-08 11:04:20 +0000

    vending_machine.py added

--- byotest.py
+++ byotest.py
@@ -10,8 +10,8 @@ def test_is_in(collection, item):
 def test_not_in(collection, item):
     assert item not in collection, "{0} contains {1}" .format(collection, item)
     
-def test_between(base, limit, item):
-    assert base <= item <= limit, "{2} is not between {0} and {1}" .format(base, limit, item)
+def test_between(lower_limit, upper_limit, item):
+    assert lower_limit <= item <= upper_limit, "{2} is not between {0} and {1}" .format(lower_limit, upper_limit, item)
 
 #test_not_equal(number_of_evens([1, 2, 3, 4, 5]), 2)
 
@@ -19,4 +19,4 @@ def test_between(base, limit, item):
 
 #test_not_in([1, 2, 3, 4, 5], 5)
 
-test_between(2, 6, 8)
+#test_between(2, 6, 8)
